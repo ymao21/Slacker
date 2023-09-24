d3b750b03912130499653f83d945db5fd576fb12
yining <yiningmaomao@gmail.com>
2023-09-24 17:12:11 -0400

    updated landing page with favicon

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -14,13 +14,56 @@ function Landing() {
                 <>
                     {redirect && <Redirect to="/chat" />}
                     <div className='landing-demo-room-container'>
-                        <div className='landing-demo-room'>
-                            <div className='landing-demo-room-title'>Demo Server</div>
-                            <div className='landing-demo-room-description'>This is a demo server</div>
-                            <div className='landing-demo-room-enter-button' onClick={() => setRedirect(true)}>Enter</div>
-                        </div>
+                        {/* <div className='landing-demo-room'> */}
+                            {/* <div className='landing-demo-room-title'>Demo Server</div> */}
+                            {/* <div className='landing-demo-room-description'>This is a demo server</div> */}
+                            <div className='landing-demo-room-enter-button' onClick={() => setRedirect(true)}>Enter Demo Server</div>
+                        {/* </div> */}
                     </div>
+
+
+                    <div className='landingpage'>
+
+<div className='image-container'>
+<img src={landingPageImage} alt="Landing Page"
+   className='landingimage' />
+
+
+<div className='text-container'>
+
+<div class="container">
+<h1 class="typed">Discover a new way of working</h1>
+</div>
+
+<img src={landingPageGif} alt="Landing gif"
+   className='landinggif' />
+
+<h2 className='landingsecondh'>All the features work together so you can too.</h2>
+
+<h2 className='landingfourthh'>
+Bring the right people and information together in channels. Share ideas, make decisions and move work forward with a common purpose and place.
+</h2>
+</div>
+</div>
+</div>
+
+<div className='social-footer'>
+    <div>
+        <div className='social-item'>
+            <a href='https://github.com/ymao21' class="github-button" >
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt='github' />
+
+            Yining Mao
+            </a>
+        </div>
+
+
+    </div>
+
+</div>
                 </>
+
+
             );
         if (!user)
             return (
@@ -47,18 +90,20 @@ function Landing() {
           Bring the right people and information together in channels. Share ideas, make decisions and move work forward with a common purpose and place.
         </h2>
       </div>
-
-
     </div>
-
   </div>
+
                     <div className='social-footer'>
                         <div>
                             <div className='social-item'>
-                                <a href='https://github.com/ymao21' target='_blank' >
-                                    <img src='https://shields.io/badge/GitHub-Yining%20Mao-400d40?logo=github&style=plastic' alt='github' />
+                                <a href='https://github.com/ymao21' class="github-button" >
+                                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt='github' />
+
+                                Yining Mao
                                 </a>
                             </div>
+
+
                         </div>
 
                     </div>
